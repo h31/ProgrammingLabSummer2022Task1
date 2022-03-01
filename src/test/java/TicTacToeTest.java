38aaa73b247693150bd156dbca7387747b5ed684
LeeryLis <64995672+LeeryLis@users.noreply.github.com>
2022-03-01 11:47:31 +0300

    Update TicTacToeTest.java

--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -1,9 +1,4 @@
 import org.junit.jupiter.api.Test;
-
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TicTacToeTest {
