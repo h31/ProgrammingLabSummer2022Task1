2929db2fe86d1b751b6e71d41ef1d72ade9d654e
amersmer <n.e.grigore17@gmail.com>
2022-03-18 18:24:45 +0300

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        Book f = new Book("ПП", "Egor", "gachi", "A3");
-        Library l = new Library();
-        l.addBook(f);
-        f.setName("->");
-        l.addBook(f);
+        Book first = new Book("GG", "Egor", "gachi", "A3");
+        Book second = new Book("Sasha Lukichev", "Egor", "gachi", "A3");
+        Book third = new Book("GG", "Egor", "gachi", "A4");
+        Library lib = new Library(first, second);
+        lib.addBook(third);
+        lib.allBooks();
     }
 }
